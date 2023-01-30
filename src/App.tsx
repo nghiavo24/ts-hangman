f5b8089304f57820d59b6c34be5a4de5e9f72c81
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-29 20:29:57 -0600

    announcement added

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import { useState } from "react";
-import words from './wordList.json'
+import words from './wordList.json';
+import style from './App.css'
 
 function App() {
   const[wordToGuess, setWordToGuess] = useState(() => {
     return words[Math.floor(Math.random() * words.length)]
   })
 
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([])
 
-  
-  return <h1>Hello World</h1>
+  return(
+    <div className="main-container">
+      <div className="announcement">
+        Lose
+        Win
+      </div>
+
+    </div>
+  )
 }
 
 export default App
