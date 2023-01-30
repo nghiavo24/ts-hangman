233cf796ffad58d17cefa1957165fa15df131de3
nghiavo24 <nghia.vo1452@gmail.com>
2023-01-29 20:15:42 -0600

    Added function to randomly pick out word from list

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+import words from './wordList.json'
+
 function App() {
+  const[wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)]
+  })
+
+
+  
   return <h1>Hello World</h1>
 }
 
